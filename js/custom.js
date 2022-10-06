d9a47428ae0873274d0c7f40ddb94fd7cd737755
vchunn <111947696+vchunn@users.noreply.github.com>
2022-10-06 20:36:56 +0700

    upload

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.element', {
+               	strings: ["Fresher Digital Artist", "Fresher Graphics Designer", "Fresher Video Editor","Fresher Web Designer"],
+       	 		typeSpeed:50,
+        		backSpeed:50,
+        		loop:true,
+        		});
+
